28433fc5617570e775995c235f1733f1857f9fe0
Karl Beecken <karl@beecken.berlin>
2020-06-16 17:40:25 +0200

    add mockup api + mongodb

--- app.js
+++ app.js
@@ -2,19 +2,29 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var bodyParser= require('body-parser')
+var bodyParser= require('body-parser');
+var cors = require('cors');
+var MongoClient = require('mongodb').MongoClient;
 
 var createRouter = require('./routes/create');
 var listRouter = require('./routes/list');
 
 var app = express();
 
+var connectionString = "";
+
+MongoClient.connect(connectionString, (err, client) => {
+    if (err) return console.error(err);
+    console.log('Connected to Database');
+});
+
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded({ extended: true }))
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
 app.use('/create', createRouter);
 app.use('/list', listRouter);
--- routes/create.js
+++ routes/create.js
@@ -2,7 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-  res.json({ title: 'Express' });
+  res.json({ message: 'This is a POST endpoint.' });
 });
+
+router.post('/', (req, res) => {
+  console.log(req.body)
+})
 
 module.exports = router;
